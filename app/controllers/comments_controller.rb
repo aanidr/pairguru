3cafaa7b85c31748607049c41c385f2cc144ed33
aanidr <nawadr96@gmail.com>
2018-08-02 11:38:36 +0200

    Added unique constraint to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
-		@comment = Comment.create(params.require(:comment).permit(:user_id, :movie_id, :content))
+		@comment = Comment.new(params.require(:comment).permit(:user_id, :movie_id, :content))
+		flash[:notice] = 'You can\'t have two comments under one movie, please delete old comment first!' if !@comment.save
 		redirect_to "/movies/#{params[:movie_id]}"
   end
 
