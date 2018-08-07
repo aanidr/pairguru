4710e1d95b938c987a9336aa014a9b4fd205621e
aanidr <nawadr96@gmail.com>
2018-08-07 22:15:55 +0200

    Validator refactored

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -5,20 +5,37 @@ class TitleBracketsValidator < ActiveModel::Validator
 	CLOSING_BRACKETS = [')', ']', '}']
 
   def validate(record)
-		expected = []
-		just_opened = false #Needed to check if opened bracket is empty
+		@expected = [] #stack needed to keep order of brackets
+		@just_opened = false #needed to check if bracket is empty
 		record.title.each_char do |c|
-			if !(id = OPENING_BRACKETS.index(c)).nil?
-				expected << CLOSING_BRACKETS[id]
-				just_opened = true
-				next
-			elsif !(id = CLOSING_BRACKETS.index(c)).nil?
-				record.errors.add(:title, 'unmatched closing bracket') if expected.pop != CLOSING_BRACKETS[id]
-				record.errors.add(:title, 'empty bracket') if just_opened
+			if (id = opening_bracket?(c))
+				open_bracket(id)
+			elsif (id = closing_bracket?(c))
+				record.errors.add(:title, 'unmatched closing bracket') unless close_bracket?(id)
+				record.errors.add(:title, 'empty bracket') if @just_opened
 			else
-				just_opened = false
+				@just_opened = false
 			end
 		end
-		record.errors.add(:title, 'unmatched opening bracket') if !expected.empty?
+		record.errors.add(:title, 'unmatched opening bracket') unless @expected.empty?
+	end
+
+	private
+	def opening_bracket?(char)
+		#checks if char is in OPENING_BRACKETS and returns its position if it is not returns nil
+		OPENING_BRACKETS.index(char)
+	end
+
+	def closing_bracket?(char)
+		CLOSING_BRACKETS.index(char)
+	end
+
+	def open_bracket(bracket_id)
+		@expected << CLOSING_BRACKETS[bracket_id]
+		@just_opened = true
+	end
+	
+	def close_bracket?(bracket_id)
+		@expected.pop == CLOSING_BRACKETS[bracket_id]
 	end
 end
