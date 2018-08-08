9ca1b85054052409b43b0790fbd2a6e555fcddb0
aanidr <nawadr96@gmail.com>
2018-08-08 13:49:32 +0200

    Made top commenters public

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:top_commenters]
 
   def create
 		movie = Movie.find(params[:movie_id])
